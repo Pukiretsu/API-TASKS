81172f22b444f8e7e43c5f4c712c021cc14766a3
Angel <cr3pym4st3r@gmail.com>
2025-11-30 10:08:13 -0500

    Tasks: view routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskViewController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Redirige la ruta raíz a la lista de tareas
+Route::redirect('/', '/tasks');
+
+Route::controller(TaskViewController::class)->group(function () {
+    // 1. Ruta para la lista de tareas: /tasks
+    Route::get('/tasks', 'index')->name('tasks.index');
+
+    // 2. Ruta para el formulario de creación: /tasks/create
+    Route::get('/tasks/create', 'create')->name('tasks.create');
+
+    // 3. Ruta para el formulario de edición: /tasks/{task}/edit
+    Route::get('/tasks/{taskId}/edit', 'edit')->name('tasks.edit');
 });
