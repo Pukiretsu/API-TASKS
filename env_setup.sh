145ee8a64795a5e8d32b5248ac26b9cfd1c15949
Angel Gonzalez <cr3pym4st3r@gmail.com>
2025-12-01 06:59:00 -0500

    Finx typo env_setup .env to env

--- env_setup.sh
+++ env_setup.sh
@@ -3,16 +3,16 @@
 
 echo "Iniciando configuración de archivos de entorno..."
 
-if [ -f "./.env.raiz" ]; then
-    cp ./.env.raiz .env
+if [ -f "./env.raiz" ]; then
+    cp ./env.raiz .env
     echo "Archivo .env de la raíz creado/actualizado correctamente."
 else
     echo "ERROR: No se encontró '.env.raiz'. Terminando."
     exit 1
 fi
 
-if [ -f "./.env.laravel" ]; then
-    cp ./.env.laravel src/.env
+if [ -f "./env.laravel" ]; then
+    cp ./env.laravel src/.env
     echo "Archivo .env de Laravel (en src/) creado/actualizado correctamente."
 else
     echo "ERROR: No se encontró '.env.laravel'. Terminando."
